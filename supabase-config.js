08feea3c2a3de30eacfef089c82472e1d783b53d
Rzou <rzouh1p@gmail.com>
2026-07-27 20:50:21 -0400

    Add Supabase public configuration

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.HOUHOU_SUPABASE = {
+  url: "https://iiyhgcafsoextydgjvme.supabase.co",
+  publishableKey: "sb_publishable_Lw8i6f_H3WKtkPDmwtqBMw_OzLGM_gc",
+  adminEmail: "rzouh1p@gmail.com"
+};
